32b23f5eae044d8bdce0b1f1a2b595f0998cc90f
falsisdev <keremtokul@hotmail.com>
2026-06-29 10:50:11 +0300

    feat: initialize echo framework and set up basic route

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	fmt.Println("Mangile Backend başlıyor...")
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Mangile Backend Çalışıyor!")
+	})
+
+	e.Logger.Fatal(e.Start(":3000"))
 }
