64224eff6ee7ff17d9e4cf75e6ae5418acc6e458
falsisdev <keremtokul@hotmail.com>
2026-06-29 10:56:12 +0300

    feat: add godotenv for environment variable management and update root response message

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("[HATA]: .env dosyası yüklenirken bir hata oluştu.")
+	}
+
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Mangile Backend Çalışıyor!")
+		return c.String(http.StatusOK, "[✅]: Sunucu başarıyla başlatıldı.")
 	})
 
 	e.Logger.Fatal(e.Start(":3000"))
