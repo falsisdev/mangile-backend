fc04aaa547204e9e4aee27f4dc52cf8b38e9a58b
Falsis <keremtokul@hotmail.com>
2026-08-12 12:32:32 +0300

    feat: refactor User model and update GetUser query to include roles and integrations

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,22 +1,16 @@
 package models
 
 type User struct {
-	ID          string        `json:"_id"`
-	LogtoID     string        `json:"logtoId"`
-	Type        string        `json:"_type"` //auth
-	CreatedAt   string        `json:"_createdAt"`
-	Avatar      string        `json:"avatar"`
-	BannerImage string        `json:"banner"`
-	FavChapters []interface{} `json:"favoriteChapters"`
-	Biography   string        `json:"bio"`
-	FavScans    []interface{} `json:"favoriteScans"`
-	FavTitle    string        `json:"favoriteTitle"`
-	FavTitles   []string      `json:"favoriteTitles"`
-	Gender      string        `json:"gender"`
-	Lists       []interface{} `json:"lists"`
-	Name        string        `json:"name"`
-	Username    string        `json:"username"`
+	ID           string        `json:"_id"`
+	LogtoID      string        `json:"logtoId"`
+	Type         string        `json:"_type"` //auth
+	CreatedAt    string        `json:"_createdAt"`
+	Avatar       string        `json:"avatar"`
+	BannerImage  string        `json:"banner"`
+	Biography    string        `json:"bio"`
+	Roles        []string      `json:"roles"`
+	Lists        []interface{} `json:"lists"`
+	Integrations []interface{} `json:"integrations"`
+	Name         string        `json:"name"`
+	Username     string        `json:"username"`
 }
-
-//Bu sürümde bookcase nesnesi tamamen kaldırıldı
-//Onun yerine Anilist/MAL Sync eklenecek ilerleyen dönemlerde
